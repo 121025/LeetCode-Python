3e10a64d15966b2534dab2822f65c28c838e2279
121025 <june0199@naver.com>
2021-08-06 01:27:02 +0900

    Update 1.py

--- 0125. Valid Palindrome/1.py
+++ 0125. Valid Palindrome/1.py
@@ -1,13 +1,14 @@
 #272ms
 
-def isPalindrome(self, s: str) -> bool:
-    strs = []
-    for char in s:
-        if char.isalnum():
-            strs.append(char.lower())
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
 
-    while len(strs) > 1:
-        if strs.pop(0) != strs.pop():
-            return False
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
 
-    return True
+        return True
