e86dc5f4dabba700b5c586078023dd8b42afa314
121025 <june0199@naver.com>
2021-08-06 01:26:00 +0900

    Update 2.py

--- 0001. Two Sum/2.py
+++ 0001. Two Sum/2.py
@@ -1,7 +1,8 @@
 #664ms
 
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    for i, n in enumerate(nums):
-        complement = target - n
-        if complement in nums[i + 1:]:
-            return [i, nums[i + 1:].index(complement) + i + 1]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, n in enumerate(nums):
+            complement = target - n
+            if complement in nums[i + 1:]:
+                return [i, nums[i + 1:].index(complement) + i + 1]
