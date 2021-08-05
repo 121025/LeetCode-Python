9854134447bdd633e41426d75714dd5a8be08638
121025 <june0199@naver.com>
2021-08-06 01:26:28 +0900

    Update 4.py

--- 0001. Two Sum/4.py
+++ 0001. Two Sum/4.py
@@ -1,8 +1,9 @@
 #60ms
 
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    nums_map = {}
-    for i, n in enumerate(nums):
-        if target - n in nums_map:
-            return [nums_map[target - n], i]
-        nums_map[n] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
+        for i, n in enumerate(nums):
+            if target - n in nums_map:
+                return [nums_map[target - n], i]
+            nums_map[n] = i
