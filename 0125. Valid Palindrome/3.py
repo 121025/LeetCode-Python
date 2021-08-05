dc5f6ef463322b29a53fe01704535fc26c0570d7
121025 <june0199@naver.com>
2021-08-06 01:35:02 +0900

    Create 3.py

---
+++ 0125. Valid Palindrome/3.py
@@ -0,0 +1,10 @@
+#36ms
+
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub("[^a-z0-9]", "", s)
+        
+        return s == s[::-1]
