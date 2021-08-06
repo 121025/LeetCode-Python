13d7b697e549ce80a6b622a8265383e98ec5127b
121025 <june0199@naver.com>
2021-08-07 01:29:14 +0900

    Create 1.py

---
+++ 0819. Most Common Word/1.py
@@ -0,0 +1,11 @@
+#32ms
+
+import re
+import collections
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r"[^\w]", " ", paragraph).lower().split()
+                if word not in banned]
+        counts = collections.Counter(words)
+        return counts.most_common()[0][0]
