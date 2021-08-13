8f40ff5e61592eefdb39876bef9b6f1c04005946
121025 <june0199@naver.com>
2021-08-14 02:40:02 +0900

    Create 1.py

---
+++ 0017. Letter Combinations of a Phone Number/1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        def dfs(index, path):
+            if len(path) == len(digits):
+                result.append(path)
+                return
+            
+            for i in dic[digits[index]]:
+                dfs(index + 1, path + i)
+        
+        if not digits:
+            return []
+        
+        dic = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+              "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        result = []
+        dfs(0, "")
+        
+        return result
