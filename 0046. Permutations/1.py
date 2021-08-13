a8c4a55837f5617dba57af102b11d840f1063d01
121025 <june0199@naver.com>
2021-08-14 03:03:32 +0900

    Create 1.py

---
+++ 0046. Permutations/1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        prev_elements = []
+        
+        def dfs(elements):
+            if not elements:
+                results.append(prev_elements[:])
+                
+            for e in elements:
+                next_elements = elements[:]
+                next_elements.remove(e)
+                
+                prev_elements.append(e)
+                dfs(next_elements)
+                prev_elements.pop()
+                
+        dfs(nums)
+        return results
